bb3df55be0957960c8e9596f7d7fa77238e09a97
Matthew Bresette <bresettem@gmail.com>
2020-02-23 21:27:48 -0600

    Names

--- app/helpers/index_helper.rb
+++ app/helpers/index_helper.rb
@@ -1,2 +1,21 @@
 module IndexHelper
+  def male(letter)
+    male         = Faker::Name.male_first_name
+    first_letter = male[0, 1]
+    while letter != first_letter
+      male         = Faker::Name.male_first_name
+      first_letter = male[0, 1]
+    end
+    male
+  end
+
+  def female(letter)
+    female         = Faker::Name.female_first_name
+    first_letter = female[0, 1]
+    while letter != first_letter
+      female         = Faker::Name.female_first_name
+      first_letter = female[0, 1]
+    end
+    female
+  end
 end
